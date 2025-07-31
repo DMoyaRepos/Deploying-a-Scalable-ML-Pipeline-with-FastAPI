f9d63a6527717a6c2690ea2d19a4eaf9fb10d96f
David Moya <david.moya1523@gmail.com>
2025-07-31 00:05:05 -0500

    fixed defect in main.py and uploaded a new screenshots/local_api.png

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def post_inference(data: Data):
     data_processed, _, _, _ = process_data(
         data,
         categorical_features=cat_features,
-        label="salary",
+        label=None,
         training=False,
         encoder=encoder
     )
